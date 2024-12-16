ebd5a31d52383f9c997b353963a3387353593d87
Mike Guida <mike@mguida.com>
2024-12-16 11:31:02 -0700

    update method comments for QueryResultCache.ts

--- src/cache/QueryResultCache.ts
+++ src/cache/QueryResultCache.ts
@@ -16,13 +16,15 @@ export interface QueryResultCache {
     disconnect(): Promise<void>
 
     /**
-     * Performs operations needs to be created during schema synchronization.
+     * Perform operations during schema synchronization.
      */
     synchronize(queryRunner?: QueryRunner): Promise<void>
 
     /**
-     * Caches given query result.
-     */
+    * Get data from cache.
+    * Returns cache result if found.
+    * Returns undefined if result is not cached.
+    */
     getFromCache(
         options: QueryResultCacheOptions,
         queryRunner?: QueryRunner,
@@ -48,7 +50,7 @@ export interface QueryResultCache {
     clear(queryRunner?: QueryRunner): Promise<void>
 
     /**
-     * Removes all cached results by given identifiers from cache.
+     * Removes all cached results by given identifiers from the cache.
      */
     remove(identifiers: string[], queryRunner?: QueryRunner): Promise<void>
 }
